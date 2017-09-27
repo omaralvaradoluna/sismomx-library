026839bd520f65eeaa0b8cc5aae970d11eb724aa
Omar Alvarado <oalvarado@conlana.com>
2017-09-26 19:30:45 -0500

    #30 Se limita construccion de encodedkey a determinados campos en link

--- src/Builders/LinkBuilder.php
+++ src/Builders/LinkBuilder.php
@@ -35,6 +35,7 @@ class LinkBuilder extends BuilderAbstract implements LinkBuilderInterface
      */
     public function internalBuild()
     {
+        $this->buildEncodedKey();
         $this->builtable->id = $this->values->getValue(LinkDictionary::ID);
         $this->builtable->description = $this->values->getValue(LinkDictionary::DESCRIPTION);
         $this->builtable->url = $this->values->getValue(LinkDictionary::URL);
@@ -43,7 +44,19 @@ class LinkBuilder extends BuilderAbstract implements LinkBuilderInterface
             'Y-m-d H:i:s',
             'now'
         );
-        $this->builtable->encodedKey = hash('sha256',json_encode($this->values->getValues()));
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    protected function buildEncodedKey()
+    {
+        $preKey = [
+            LinkDictionary::DESCRIPTION => $this->values->getValue(LinkDictionary::DESCRIPTION),
+            LinkDictionary::URL => $this->values->getValue(LinkDictionary::URL),
+        ];
+        $this->builtable->encodedKey = hash('sha256',json_encode($preKey));
         return $this;
     }
 }
